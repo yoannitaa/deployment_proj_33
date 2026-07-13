864c33df218c1a9a5feefad82cfd4bbc95ace90c
yoannitaa <nitanitaharlandi@gmail.com>
2026-07-13 13:38:28 +0700

    Add files via upload

--- app/utils/model_helper.py
+++ app/utils/model_helper.py
@@ -11,5 +11,5 @@ def predict(data: pd.DataFrame):
     predict_proba = model.predict_proba(data)[:,1]
     predict_proba = predict_proba.tolist()
 
-    predict_class = [1 if val >= 5 else 0 for val in predict_proba]
+    predict_class = [1 if val >= 0.5 else 0 for val in predict_proba]
     return{"class": predict_class, "prob":predict_proba}
